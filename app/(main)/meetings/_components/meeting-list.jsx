e94de830b2ad9d53e998ae9124826ca95dd2ca09
Waghre Abhishek <waghreabhishek@gmail.com>
2025-06-20 21:09:59 +0530

    update time xone setting

--- app/(main)/meetings/_components/meeting-list.jsx
+++ app/(main)/meetings/_components/meeting-list.jsx
@@ -29,14 +29,7 @@ export default function MeetingList({ meetings, type }) {
           <CardContent>
             <div className="flex items-center mb-2">
               <Calendar className="mr-2 h-4 w-4" />
-              <span>{format(new Date(meeting.startTime), "MMMM d, yyyy")}</span>
-            </div>
-            <div className="flex items-center mb-2">
-              <Clock className="mr-2 h-4 w-4" />
-              <span>
-                {format(new Date(meeting.startTime), 'h:mm a')} -{" "}
-                {format(new Date(meeting.endTime), 'h:mm a')}
-              </span>
+              <span>{format(new Date(meeting.startTime), "MMM d, yyyy h:mm a")}</span>
             </div>
             {meeting.meetLink && (
               <div className="flex items-center">
